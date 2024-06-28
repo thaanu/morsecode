8ef1beb3e270b80e83ea14cc5aa79413f5dd9d46
Ahmed Shan <ahmedshan16@gmail.com>
2024-06-29 03:09:00 +0500

    Refactored to accommodate file separation

--- golang/main.go
+++ golang/main.go
@@ -1,101 +1,19 @@
 package main
 
 import (
+	Routes "golang/routes"
 	"log"
-	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
-
-	type Response struct {
-		PlainText string `json:"plain_text"`
-	}
-
-	app := fiber.New()
-
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
-
-	app.Post("/encode", func (c *fiber.Ctx) error {
-		c.Accepts("application/json")
-
-		// todo: Validations (isset, number of characters)
-		// todo: Decode morse to plain text
-
-		r := new(Response)
-
-		if err := c.BodyParser(r); err != nil {
-			return err
-		}
-
-		encoded := morseCodeEncode( r.PlainText )
-
-		log.Println(encoded)
-
-		return c.SendString(encoded)
-	})
-
-    log.Fatal(app.Listen(":3000"))
-
-}
-
-func morseCodeEncode( plainText string ) string {
-	morseCodeCodes := map[string]string{
-		"a":".-",
-		"b":"-...", 
-		"c":"-.-.", 
-		"d":"-..", 
-		"e":".", 
-		"f":"..-.", 
-		"g":"--.", 
-		"h":"....", 
-		"i":"..", 
-		"j":".---", 
-		"k":"-.-", 
-		"l":".-..", 
-		"m":"--", 
-		"n":"-.", 
-		"o":"---", 
-		"p":".--.", 
-		"q":"--.-", 
-		"r":".-.", 
-		"s":"...", 
-		"t":"-", 
-		"u":"..-", 
-		"v":"...-", 
-		"w":".--", 
-		"x":"-..-", 
-		"y":"-.--", 
-		"z":"--..", 
-		"0":"-----",
-		"1":".----", 
-		"2":"..---", 
-		"3":"...--", 
-		"4":"....-", 
-		"5":".....", 
-		"6":"-....", 
-		"7":"--...", 
-		"8":"---..", 
-		"9":"----.",
-		".":".-.-.-",
-		",":"--..--",
-		"?":"..--..",
-		"/":"-..-.",
-		" ":" ",
-	}
-
-	lookupString := strings.Split(strings.ToLower(plainText), "")
+func init() {
 	
-	encodedMorseCode := ""
-
-	for _, char := range lookupString {
-		sMorseCode := morseCodeCodes[char]
-		encodedMorseCode = encodedMorseCode+" "+sMorseCode
-	}
-
-	return encodedMorseCode
-
 }
+
+func main() {
+	app := fiber.New()
+	Routes.Setup(app)
+    log.Fatal(app.Listen(":3000"))
+}
+
